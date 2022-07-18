ce4fe6d72302ed1c7665791c93444994d4180155
Peter Schaber <peteschaber@gmail.com>
2022-07-18 09:36:14 -0500

    install nextjs dependencies

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function HomePage() {
+    return (
+        <div>
+            <Head>
+                <title>The Fry Companion</title>
+            </Head>
+        </div>
+    )
+    
+}
+
+export default HomePage
